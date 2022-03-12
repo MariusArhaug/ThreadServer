e784e00581c5877639f4d2016bbe084bdc3d6ed1
MariusArhaug <mariusarhaug@hotmail.com>
2022-03-12 21:58:22 +0100

    DRAFT: add simple script to curl endpoint n times

---
+++ scripts/nclient.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SEQ=/bin/seq
+CURL=/bin/curl
+
+IP=localhost
+PORT=8080
+PATH=/doc/index.html
+
+URL=$IP:$PORT$PATH
+
+END=4
+
+if [ -z "$1" ]
+then
+  echo "Request number not specified, setting it to 10 requests"
+else
+  END=$1
+fi 
+
+for i in `$SEQ $END`
+do
+
+  echo "i:$i curl -S -s -o /dev/null $URL $err"
+  $CURL -S -s -o /dev/null $URL
+  
+done 
